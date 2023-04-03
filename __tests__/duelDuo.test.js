d5a2023dc270f38b94ad5cfeb35e8113940d23d8
Keldon Lopez <keldon.lopz@gmail.com>
2023-04-02 21:28:51 -0600

    completed part 4

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -1,12 +1,17 @@
 const { Builder, Browser, By, until } = require("selenium-webdriver");
 
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+
 let driver;
 
-beforeEach(async () => {
+beforeAll(async () => {
   driver = await new Builder().forBrowser(Browser.CHROME).build();
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await driver.quit();
 });
 
@@ -14,5 +19,18 @@ describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
+  });
+
+  test("shows choices after clicking draw", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id("draw")).click();
+    expect (await driver.findElement(By.id("choices")).then(function(){return true;})).toBe(true)
+  });
+
+  test("page loads with title", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id("draw")).click();
+    await driver.findElement(By.xpath(`//*[@id="choices"]/div[1]/button`)).click();
+    expect (await driver.findElement(By.id("player-duo")).then(function(){return true;})).toBe(true)
   });
 });
--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,15 +1,4 @@
 const shuffle = require('../src/shuffle')
-// const { Builder, Browser, By, until } = require("selenium-webdriver");
-
-let driver;
-
-// beforeEach(async () => {
-//   driver = await new Builder().forBrowser(Browser.CHROME).build();
-// });
-
-// afterEach(async () => {
-//   await driver.quit();
-// });
 
 let testArr = [1,2,3,4,5];
 
